1f3b90c7df5ea1cae708efe2d04267c2e7dee76c
shiju shaji <shijujeevzz@gmail.com>
2022-09-27 21:48:35 +0530

    added button and data binding to the app

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityLayoutBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLayoutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_layout)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_layout)
+
+        binding.doneButton.setOnClickListener {  addNickname(it)}
+
     }
+    private fun addNickname(view: View)
+    {
+        binding.apply {
+            nickNameText.text =editNickname.text
+            invalidateAll()
+            editNickname.visibility=View.GONE
+            nickNameText.visibility= View.VISIBLE
+        }
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0 )
+    }
+
 }
+
+
+
+
+
